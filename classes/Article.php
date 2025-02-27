05f5c9393fc8232b685b1c0b46e40ee844e8818b
anjaNante <36764520+anjanante@users.noreply.github.com>
2025-02-27 15:09:10 +0300

    Create entity Article and User

---
+++ classes/Article.php
@@ -0,0 +1,64 @@
+<?php
+namespace Classes;
+
+class Article {
+    private $id;
+
+    private $title;
+
+    private $content;
+
+    private $image;
+
+    private $date;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+}
