bf5e969dfe0fc0673777da652c3b102535311f24
anjaNante <36764520+anjanante@users.noreply.github.com>
2025-02-27 14:36:49 +0300

    Add Db class

---
+++ classes/Db.php
@@ -0,0 +1,34 @@
+<?php
+
+class Db {
+
+    public static $host = "localhost";
+
+    public static $db_name = "poo_db_pp";
+
+    public static $user = "root";
+
+    public static $password = "";
+
+    public static $port = "3306";
+
+    public static function connect()
+    {
+
+        try {
+
+            $connexion = new PDO('mysql:host='.self::$host.';port='.self::$port.';dbname='.self::$db_name.'', self::$user, self::$password);
+             
+            $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+         
+            return $connexion;
+
+        } catch(PDOException $e)
+        {
+            echo "An error has occurred during connection :".$e->getMessage();
+        }
+
+    }
+}
+
+print_r(Db::connect());
